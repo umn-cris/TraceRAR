e0bceaf071f4282f41a731048e80a3dd4f13e245
libingzheren <libingzheren@gmail.com>
2017-11-25 14:03:53 -0600

    Add files via upload

---
+++ AIX_86/bin/run.sh
@@ -0,0 +1,11 @@
+#hfplayer
+#sample run command
+#
+
+./hfplayer -nt 1 -cfg sampleConf-sda8.csv original
+#sampleTrace.csv
+#total_small2.csv
+#total_small.csv
+#total.csv
+#original
+#total_small1.csv
