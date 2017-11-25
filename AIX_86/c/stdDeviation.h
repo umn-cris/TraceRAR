e8dea75a905036c2354e8de9aee917d8589ec911
libingzheren <libingzheren@gmail.com>
2017-11-25 14:01:45 -0600

    Add files via upload

---
+++ AIX_86/c/stdDeviation.h
@@ -0,0 +1,17 @@
+/*
+© Regents of the University of Minnesota. 
+   This software is licensed under GPL version 3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html). 
+*/
+#ifndef STDDEV_H
+#define STDDEV_H
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <strings.h>
+#include <string.h>
+#include <errno.h>
+
+#endif
