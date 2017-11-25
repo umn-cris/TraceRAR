35207604afe5cc56f5d261c68a863ea32a3e77cf
libingzheren <libingzheren@gmail.com>
2017-11-25 14:19:05 -0600

    Add files via upload

---
+++ Linux_x86/bin/run.sh
@@ -0,0 +1,18 @@
+#hfplayer
+#sample run command
+#
+
+sudo ./hfplayer -nt 1 -rar 1 -cfg sampleConf-sda8.csv ~/Dropbox/IO_replay/Trace/sampleTrace.csv
+#iotcdw_config_HDD.csv
+#sampleConf-sda8.csv
+#sampleTrace.csv
+#total_small.csv
+#total.csv
+#original
+#total_small1.csv
+#total10f.csv
+#batch.hf/batch.hf
+#iotcdw.hf.500k
+#~/Dropbox/IO_replay/Trace/sampleTrace.csv
+#sampleTrace10000
+#4 -reg 300000
